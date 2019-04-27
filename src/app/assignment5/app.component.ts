9f8e4a5799c5da002ffc36c1447fc5ac9691e1b6
Shashank Chaudhry <shashch@shashch-macbookpro.roam.corp.google.com>
2019-04-27 16:50:29 -0700

    Clean up assn 5.

--- src/app/assignment5/app.component.ts
+++ src/app/assignment5/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [],
+  providers: [UsersService],
 })
 export class AppComponent {
 }
